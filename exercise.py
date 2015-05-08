a6d7386eb8fad515457b5a62a0b5a80f89946688
meliissagr <15mgutierrez@iaa.edu.ec>
2015-05-08 08:20:11 -0500

    Create exercise.py

---
+++ exercise.py
@@ -0,0 +1,15 @@
+mystring = "hello"
+myfloat = 10.0
+myint = 20.20
+
+print mystring
+print myfloat
+print myint
+
+print mystring, "is my favorite word,", myfloat, "is my favorite number,", "and so is,", myint,
+
+print myint + myfloat
+print myint - myfloat
+print myint * myfloat
+print myint / myfloat
+print myint * 16 + myfloat
