72e22e43eac9adec934ac52d73e58fcaf324dbf0
meliissagr <15mgutierrez@iaa.edu.ec>
2015-05-08 08:21:32 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,11 @@
+operand1 = 95
+operand2 = 64.5 
+
+print operand1, "+", operand2, "=", operand1 + operand2
+print operand1, "-", operand2, "=", operand1 - operand2
+print operand1, "*", operand2, "=", operand1 * operand2
+print operand1, "/", operand2, "=", operand1 / operand2
+print operand1, "%", operand2, "=", operand1 % operand2
+
+#I solved this problem by making the numbers equal to operand1 and operand2 in order for the system to respond correctly and solve the math equations
+#Writing print was a necesssary step because without it the problems wouldn't show up. 
