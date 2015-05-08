7a9190e8d549bfe671ddcaba3321dffeb5762688
meliissagr <15mgutierrez@iaa.edu.ec>
2015-05-08 08:20:33 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,17 @@
+#Practice # 1 
+#Create 5 variables that store your personal info
+#Name, Last name, Address, Email, Phone number, 
+#Print them separately and then concatenate them in a string.string
+Name = 'Melissa'
+LastName = 'Gutierrez'
+Address = 'Samborondon'
+Email = '15mgutierrez@iaa.edu.ec' 
+PhoneNumber = 2836880 
+
+print Name
+print LastName
+print Address
+print Email
+print PhoneNumber
+
+print 'My name is', Name, LastName, '. I live in', Address, '. My email is', Email, 'and phone number is', PhoneNumber
