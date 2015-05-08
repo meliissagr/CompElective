2419b4ecb291a2061fd3f8265e533b9794b60038
meliissagr <15mgutierrez@iaa.edu.ec>
2015-05-08 08:21:04 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,25 @@
+name = 'Melissa Gutierrez'
+age = 18
+gpa = 4.0
+
+print name
+print age 
+print gpa
+
+print 'My name is', name, '. I am ' , age  , 'years old.'
+
+print 'In ten years, I will be', age + 10
+
+#Dog years 
+ageInHumanYears = 8 
+ageInDogYears = ageInHumanYears * 7
+print 'My dog is', ageInDogYears, 'years old.'
+print 'But my dog is', ageInHumanYears, 'years old in Human Years.'
+
+MyNumber = 5.0 + 5 * 4
+print MyNumber
+
+#Order of Operations 
+#Parens, Exponents,  Multiplication, Division, Addiction, Subtraction
+MyNumber = 5 + 4 * 5
+print MyNumber
