e056f16fdd9e26c716e7a361fcf6785b84851f0e
meliissagr <15mgutierrez@iaa.edu.ec>
2015-05-08 08:19:35 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+print "Hello World"
+print "Welcome to programming for absolute beginners. \nThis is Python"
+print 2015 #printing an integer
+print "2015" #printing a string 
+print 3.1416 #printing a floating point number
+
+print 2 + 2
+print 2 - 2
+print 2 * 2
+print 2 / 2
+print 2 % 2
+print 3.1416 * 2 
+ 
+print 3 % 2 
+print 10 % 2
