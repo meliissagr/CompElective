efed0d5ad3b2a02afbee01d934bd6e38043d0586
meliissagr <15mgutierrez@iaa.edu.ec>
2015-05-12 08:59:59 -0500

    Create Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,16 @@
+#Create a form with the following info
+#Name
+#LastName
+#Year of Birth
+#Favorite singer/band
+#Favorite movie 
+#Best friend
+
+
+import sys
+print "My name is ", sys.argv[1]
+print "My last name is ", sys.argv[2]
+print "I was born on ", sys.argv[3]
+print "My favorite band is ", sys.argv[4] 
+print "My favorite movie is ", sys.argv[5]
+print "My best friend is ", sys.argv[6]
